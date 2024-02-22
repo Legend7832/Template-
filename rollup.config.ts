00d9d8fbb79f54c840dc257cae76930e8c0fe489
Legend7832 <128260152+Legend7832@users.noreply.github.com>
2024-02-21 16:56:02 -1000

    Update rollup.config.ts

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,24 +1,43 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import esbuild from 'rollup-plugin-esbuild';
-import json from '@rollup/plugin-json';
+import { defineConfig, Plugin } from "rollup";
+import esbuild from "rollup-plugin-esbuild";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
-import manifest from './manifest.json';
-import { defineConfig } from 'rollup';
+import { basename } from "path";
+import { writeFileSync } from "fs";
+
+const pluginName = basename(process.cwd());
 
 export default defineConfig({
-   input: 'src/index.tsx',
-   output: [
-      {
-         file: `dist/${manifest.name}.js`,
-         format: 'cjs',
-         strict: false
-      },
-   ],
-   plugins: [
-      nodeResolve(),
-      commonjs(),
-      json(),
-      esbuild({ minify: true, target: 'ES2019' })
-   ]
+  input: "src/index.ts",
+  output: [
+    {
+      file: `dist/${pluginName}.js`,
+      format: "cjs",
+      strict: false
+    },
+  ],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    esbuild({ minify: true, target: "ES2019" }),
+    createPluginJson(),
+  ]
 });
+
+function createPluginJson(options = {}): Plugin {
+  return {
+    name: 'plugin-info',
+    buildEnd: (err) => {
+      const info = require('./package.json');
+      const data = {
+        "name": pluginName,
+        "description": info?.description ?? "No description was provided.",
+        "author": info?.author?.name ?? "Unknown",
+        "version": info?.version ?? "1.0.0"
+      };
+
+      writeFileSync(`dist/${pluginName}.json`, JSON.stringify(data, null, "\t"));
+    }
+  }
+};
